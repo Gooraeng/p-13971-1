490a2ca88f2bd759f4413d4f3dadd0b62e359fff
Gooraeng <birdyoon1998@gmail.com>
2025-07-01 14:48:02 +0900

    add sample data to post list

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,15 +1,27 @@
 "use client";
 
+import { useState, useEffect } from "react";
+
 export default function Page() {
+    const [posts, setPosts] = useState<{ id: number; title: string }[]>([]);
+
+    useEffect(() => {
+        setPosts([
+            { id: 1, title: "명언 1" },
+            { id: 2, title: "명언 2" },
+            { id: 3, title: "명언 3" },
+        ]);
+    }, []);
+
     return (
         <>
             <h1>포스트 목록</h1>
             <ul>
-                <li>포스트 1</li>
-                <li>포스트 2</li>
-                <li>포스트 3</li>
+                {posts.map((post) => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
             </ul>
         </>
     );
-  }
+}
   
