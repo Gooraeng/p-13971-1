03a3ab0722d701491d36e605c4d32f5d4ec1815a
Gooraeng <birdyoon1998@gmail.com>
2025-07-01 14:12:58 +0900

    modify bg to red-500

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased !bg-red-500 border-2 border-blue-500`}
       >
         <header>
           <nav className="flex">
@@ -35,7 +35,7 @@ export default function RootLayout({
           </nav>
         </header>
         <main>{children}</main>
-        <footer>푸터</footer>
+        <footer>footer</footer>
       </body>
     </html>
   );
