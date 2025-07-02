85f91fb7d413d7f9589f768514059d86aac092b3
Gooraeng <birdyoon1998@gmail.com>
2025-07-02 10:47:02 +0900

    add delete button
    add message

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -3,11 +3,23 @@
 import { useEffect, useState, use } from "react";
 import type { PostWithContentDto } from "@/app/type/post";
 import apiFetch from "@/app/lib/backend/client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Page({ params }: { params: Promise<{ id: number }> }) {
+    const router = useRouter();
     const { id } = use(params);
 
     const [post, setPost] = useState<PostWithContentDto | null>(null);
+
+    const deletePost = (id: number) => {
+        apiFetch(`/api/v1/posts/${id}`, {
+            method: "DELETE"
+        }).then((data) => {
+            alert(data.msg);
+            router.replace("/posts");
+        })
+    }
 
     useEffect(() => {
         apiFetch(`/api/v1/posts/${id}`)
@@ -23,6 +35,15 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
             <div>번호 : {post.id}</div>
             <div>제목 : {post.title}</div>
             <div style={{ whiteSpace: "pre-line" }}>{post.content}</div>
+
+            <div className="flex gap-2">
+                <button
+                    className="border p-2 rounded"
+                    onClick={() => confirm(`${post.id}번 글을 정말로 삭제하시겠습니까?`) && deletePost(post.id)}
+                >
+                    삭제
+                </button>
+            </div>
         </>
     );
 }
