ae90174d36566fc0b52a715205088429a4f0f3a1
Gooraeng <birdyoon1998@gmail.com>
2025-07-02 11:06:52 +0900

    fix option header

--- src/app/lib/backend/client.ts
+++ src/app/lib/backend/client.ts
@@ -3,10 +3,11 @@ const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 const apiFetch = async(path: string, options?: RequestInit) => {
 
     if (options?.body) {
-        options.headers = {
-            ...options.headers,
-            "Content-Type": "application/json; charset=utf-8"
+        const headers = new Headers(options.headers);
+        if (!headers.get("Content-Type")) {
+            headers.set("Content-Type", "application/json; charset=utf-8");
         }
+        options.headers = headers;
     }
 
     return fetch(`${NEXT_PUBLIC_API_BASE_URL}${path}`, options)
